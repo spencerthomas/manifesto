a2ae31aec4b8c9369d4b09c8fdbbc534dfcc4c3f
spencerthomas <tom@cognitives.io>
2018-05-16 16:56:53 +1000

    cleaned up migrations

--- db/migrate/20180509000250_add_type_to_projects.rb
+++
@@ -1,5 +0,0 @@
-class AddTypeToProjects < ActiveRecord::Migration[5.0]
-  def change
-    add_column :projects, :type, :string
-  end
-end
--- db/migrate/20180513114126_drop_testprojects_table.rb
+++
@@ -1,9 +0,0 @@
-class DropTestprojectsTable < ActiveRecord::Migration[5.0]
-	  def change
-	  	drop_table :testprojects 
-	  end
-
-	  def down
-	  	fail ActiveRecord::IrreversibleMigration 
-	  end
-end
